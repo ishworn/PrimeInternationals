ff016d8ac4a85cc9ca8a011375142469c2d1d0d6
ishwor <ishwornepal1234@gmail.com>
2025-06-09 12:55:05 +0545

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,10 @@ class DatabaseSeeder extends Seeder
 
     public function run()
 {
-
+   $this->call(PermissionSeeder::class);
     $this->call(RoleSeeder::class);
+    $this->call(UserSeeder::class);
+ 
+   
 }
 }
